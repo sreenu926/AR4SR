85299ee1b70512598cfe43ba58d49bfaf1532dc6
GreatStackDev <your-email@example.com>
2025-06-01 17:06:46 +0530

    removed unnecessary files

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,12 +1,9 @@
 "use client";
 import React, { useState } from "react";
 import LogoTitle from "./_components/exercises";
-import { ArrowLeft, ArrowRight } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import LogoColorPallete from "./_components/LogoColorPallete";
 import LogoDesigns from "./_components/LogoDesigns";
 import LogoIdea from "./_components/LogoIdea";
-import PricingModel from "./_components/PricingModel";
 
 function CreateLogo() {
   const [step, setStep] = useState(1);
@@ -39,32 +36,7 @@ function CreateLogo() {
           onHandleInputChange={(v) => onHandleInputChange("idea", v)}
           formData={formData}
         />
-      ) : step == 5 ? (
-        <PricingModel
-          formData={formData}
-          onHandleInputChange={(v) => onHandleInputChange("pricing", v)}
-        />
       ) : null}
-
-      {/* <div className="flex items-center justify-between mt-10">
-        {step != 1 && (
-          <div className="border-2 border-white hover:rounded-sm hover:border-black">
-            <Button onClick={() => setStep(step - 1)}>
-              <div className="flex gap-4 cursor-pointer">
-                <ArrowLeft /> <span className="hidden md:inline">Previous</span>
-              </div>
-            </Button>
-          </div>
-        )}
-
-        <div className="border-2 border-white hover:rounded-sm hover:border-black">
-          <Button onClick={() => setStep(step + 1)} className="bg-red-500">
-            <div className="flex gap-4 cursor-pointer">
-              <span className="hidden md:inline">Continue</span> <ArrowRight />
-            </div>
-          </Button>{" "}
-        </div>
-      </div> */}
     </div>
   );
 }

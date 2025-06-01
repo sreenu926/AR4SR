f4fbf0f058f62fc315f871a32c0e8d57df4859e7
sreenu926 <89065670+sreenu926@users.noreply.github.com>
2025-06-01 16:46:39 +0530

    Update next.config.mjs
    deleted comments

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["firebasestorage.googleapis.com"],
-//   },
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // No need to configure external image domains since you're using local images
